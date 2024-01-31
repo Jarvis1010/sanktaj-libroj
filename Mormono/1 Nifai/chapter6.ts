b883a2ee80f0c7e7f7b5b299851d4af7f9ad3b84
Jarvis1010 <travis.mair@gmail.com>
2024-01-30 21:26:21 -0600

    add 1st nifai

---
+++ Mormono/1 Nifai/chapter6.ts
@@ -0,0 +1,12 @@
+export const chapter6 = {
+  chapterTitle: "Ĉapitro 6",
+  summary: "",
+  verses: [
+    "Kaj nun mi, Nifai, ne donas la genealogion de miaj prapatroj en ĉi tiu parto de mia kroniko; kaj mi neniam donos ĝin poste sur ĉi tiuj platoj, kiujn mi surskribas; ĉar ĝi estas donita en la kroniko, kiun mia patro verkis; pro tio mi ne skribas ĝin en ĉi tiu verko.",
+    "Ĉar sufiĉas por mi diri, ke ni estas idoj de Jozef.",
+    "Kaj neniel gravas al mi, ke mi zorge donu plenan raporton pri ĉiuj aferoj de mia patro, ĉar ili ne povas esti skribataj sur ĉi tiujn platojn, kaj mi volas la spacon, por ke mi skribu pri la aferoj de Dio.",
+    "Ĉar mia plena intenco estas, ke mi povu persvadi homojn veni al la Dio de Abraham kaj la Dio de Isaak kaj la Dio de Jakob kaj esti savitaj.",
+    "Tial tion, kio plaĉas al la mondo, mi ne skribas, sed tion, kio plaĉas al Dio kaj al tiuj, kiuj ne estas de ĉi tiu mondo.",
+    "Tial mi donos ordonon al mia idaro, ke ĝi ne plenigu ĉi tiujn platojn per aferoj, kiuj ne valoras por la homidoj.",
+  ],
+} as const satisfies Chapter;
--- Mormono/1 Nifai/index.ts
+++ Mormono/1 Nifai/index.ts
@@ -1,5 +1,13 @@
 import { chapter1 } from "./chapter1";
 import { chapter2 } from "./chapter2";
+import { chapter3 } from "./chapter3";
+import { chapter4 } from "./chapter4";
+import { chapter5 } from "./chapter5";
+import { chapter6 } from "./chapter6";
+import { chapter7 } from "./chapter7";
+import { chapter16 } from "./chapter16";
+import { chapter17 } from "./chapter17";
+import { chapter18 } from "./chapter18";
 
 export const unuNifai = {
   bookTitle: "La Unua Libro De Nifai",
@@ -7,5 +15,16 @@ export const unuNifai = {
   subtitle: "Lia regado kaj lia servado",
   summary:
     "Rakonto de Lehi, kaj lia edzino Sariah, kaj liaj kvar filoj nomitaj, komencante per la plej maljuna, Laman, Lemuel, Sam, kaj Nephi. La Eternulo avertas Lehion, ke li eliru la landon Jerusaleman ĉar li profetis al la popolo pri ilia maljusteco kaj ili celas detrui lian vivon. Li entreprenas vojaĝon tri tagojn en la sovaĝejo. Nephi prenas siajn fratojn kaj reiras al la lando Jerusalema celante la kronikon de la judoj. La rakonto de iliaj suferadoj. Ili edzinigas al si la filinojn de Iŝmael. Ili prenas siajn familiojn kaj eliras en la sovaĝejon. La direkto de iliajn vojaĝadojn. Ili venas al la grandaj akvoj. La fratoj de Nephi ribelas kontraŭ li. Li hontigas ilin kaj konstruas ŝipon. Ili nomas la lokon Fruktoriĉo. Ili transiras la grandajn akvojn en la promesitan landon, ktp. Ĉi tio estas laŭ la rakonto de Nephi, alivorte, mi, Nephi, skribis ĉi tiun kronikon.",
-  chapters: [chapter1, chapter2],
+  chapters: [
+    chapter1,
+    chapter2,
+    chapter3,
+    chapter4,
+    chapter5,
+    chapter6,
+    chapter7,
+    chapter16,
+    chapter17,
+    chapter18,
+  ],
 } as const satisfies Book;
