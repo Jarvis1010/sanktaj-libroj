8a9589afc7c1c61fa0c33c59251c650d019ae9b0
Jarvis1010 <travis.mair@gmail.com>
2024-03-25 15:58:48 -0500

    add more chapters

--- MultevaloraPerlo/Moseo/index.ts
+++ MultevaloraPerlo/Moseo/index.ts
@@ -1,8 +1,11 @@
 import { chapter1 } from "./chapter1";
+import { chapter2 } from "./chapter2";
+import { chapter3 } from "./chapter3";
+import { chapter4 } from "./chapter4";
 
 export const moseo = {
   bookTitle: "La Libro De Moseo",
   bookTitleShort: "Moseo",
   subtitle: "Vizioj de Moseo",
-  chapters: [chapter1],
+  chapters: [chapter1, chapter2, chapter3, chapter4],
 } as const satisfies Book;
