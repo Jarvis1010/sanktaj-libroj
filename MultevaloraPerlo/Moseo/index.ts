7375a4d35184c8d61a6a9246cd89feb13fd7f9ac
Jarvis1010 <travis.mair@gmail.com>
2025-01-14 10:33:39 -0600

    moses chapter 6

--- MultevaloraPerlo/Moseo/index.ts
+++ MultevaloraPerlo/Moseo/index.ts
@@ -3,10 +3,11 @@ import { chapter2 } from "./chapter2";
 import { chapter3 } from "./chapter3";
 import { chapter4 } from "./chapter4";
 import { chapter5 } from "./chapter5";
+import { chapter6 } from "./chapter6";
 
 export const moseo = {
   bookTitle: "La Libro De Moseo",
   bookTitleShort: "Moseo",
   subtitle: "Vizioj de Moseo",
-  chapters: [chapter1, chapter2, chapter3, chapter4, chapter5],
+  chapters: [chapter1, chapter2, chapter3, chapter4, chapter5, chapter6],
 } as const satisfies Book;
